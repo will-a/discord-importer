2df77a44d687e2f3cb34c98f75fd6884717ee032
Will Anderson <willa@fastmail.com>
2022-03-25 23:25:55 -0700

    #2 Add superuser functions to ingest all messages in channel, toggle verbose

--- importer.py
+++ importer.py
@@ -1,6 +1,7 @@
 import json
 import discord
 import requests
+requests.packages.urllib3.disable_warnings()
 
 
 # read configs
@@ -21,27 +22,19 @@ if elastic_token is None:
     print("ERROR: No Elastic token found in configs.json")
     exit(1)
 
-admins = configs.get('admins')
-if admins is None:
-    print("WARNING: No admins specified in configs.json")
+superusers = configs.get('superusers')
+if superusers is None:
+    print("WARNING: No superusers specified in configs.json")
 
 elastic_base_url = configs.get('host')
 if elastic_base_url is None:
     print("ERROR: No host found in configs.json")
 
+verbose = False
 client = discord.Client()
 
 
-@client.event
-async def on_ready():
-    print("Logged in as {user}".format(user=client.user))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
+async def post_message_to_elastic(message:discord.Message, verbose:bool=False) -> requests.Response:
     payload = {
         '@timestamp': message.created_at.strftime('%Y-%m-%dT%H:%M:%SZ'),
         'author': str(message.author),
@@ -53,8 +46,48 @@ async def on_message(message):
         'attachments': [{'type': attachment.content_type, 'url': attachment.url } for attachment in message.attachments]
     }
 
-    resp = requests.post(url=elastic_base_url + 'hideout/_doc', data=json.dumps(payload), headers={'Content-Type': 'application/json','Authorization': "ApiKey {}".format(elastic_token)}, verify=False)
+    resp = requests.post(
+        url=elastic_base_url + 'hideout/_doc',
+        data=json.dumps(payload),
+        headers={'Content-Type': 'application/json','Authorization': "ApiKey {}".format(elastic_token)},
+        verify=False
+    )
 
-    await message.channel.send("{}: {}".format(resp.status_code, resp.content))
+    if verbose:
+        await message.channel.send("{}: {}".format(resp.status_code, resp.content))
+
+
+@client.event
+async def on_ready():
+    print("Logged in as {user}".format(user=client.user))
+
+
+@client.event
+async def on_message(message):
+    global verbose
+    if message.author == client.user:
+        return
+    
+    if message.content is not None and message.content.startswith('!') and len(message.content) > 1 and message.author.id in superusers:
+        command = message.content[1:].split(' ')[0]
+        
+        if command == 'ingest':
+            await message.channel.send("Ingesting messages from {channel}...".format(channel=message.channel.name))
+            message_count = 0
+            async for historic_message in message.channel.history(limit=None):
+                if historic_message.author != client.user:
+                    await post_message_to_elastic(historic_message)
+                    message_count += 1
+            
+            await message.channel.send("Successfully ingested {num_messages} messages from {channel}.".format(
+                num_messages=message_count, channel=message.channel.name
+            ))
+            return
+        elif command == 'verbose':
+            verbose = False if verbose else True
+            await message.channel.send("Verbose toggled to {verbose}.".format(verbose=verbose))
+            return
+
+    await post_message_to_elastic(message, verbose)
 
 client.run(discord_token)

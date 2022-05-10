03b128831f10253fb818b0a469cdaf09aeddff7a
Will Anderson <willanderson@Wills-MacBook-Pro.local>
2022-05-09 23:05:10 -0400

    GH-17 No longer using f-strings in logging messages

--- importer.py
+++ importer.py
@@ -1,51 +1,68 @@
-# Author: Will Anderson (@will-a)
+""" Author: Will Anderson (@will-a) """
 
+import sys
 import json
-import discord
 import logging
-import requests
 from pathlib import Path
+
+import discord
+import requests
+
 requests.packages.urllib3.disable_warnings()
 
 logging.basicConfig(level=logging.INFO)
-root = str(Path(__file__).parent)
+ROOT = str(Path(__file__).parent)
 
 # read configs
 try:
-    with open(f'{root}/configs.json', 'r') as configs_file:
+    with open(f'{ROOT}/configs.json', 'r', encoding='utf-8') as configs_file:
         configs = json.loads(configs_file.read())
 except OSError:
     logging.error("ERROR: could not read configs.json")
-    exit(1)
+    sys.exit(1)
 
 discord_token = configs.get('discord_token')
 if discord_token is None:
     logging.error("ERROR: No bot token found in configs.json")
-    exit(1)
+    sys.exit(1)
 
 elastic_token = configs.get('elastic_token')
 if elastic_token is None:
     logging.error("ERROR: No Elastic token found in configs.json")
-    exit(1)
+    sys.exit(1)
 
 superusers = configs.get('superusers')
 if superusers is None:
-    logging.warn("WARNING: No superusers specified in configs.json")
+    logging.warning("WARNING: No superusers specified in configs.json")
 
 elastic_base_url = configs.get('host')
 if elastic_base_url is None:
     logging.error("ERROR: No host found in configs.json")
-    exit(1)
+    sys.exit(1)
 
 elastic_index = configs.get('index')
 if elastic_index is None:
     logging.error("ERROR: Elastic index not provided")
-    exit(1)
+    sys.exit(1)
 
 verbose = False
 client = discord.Client()
 
-async def post_message_to_elastic(message:discord.Message, verbose:bool=False) -> requests.Response:
+
+async def post_message_to_elastic(message: discord.Message) -> requests.Response:
+    """
+    Post Discord message to Elastic database
+
+    params
+    ------
+    message : discord.Message
+        discord.Message object ingested by client.
+
+    return
+    ------
+    requests.Response:
+        Response from Elastic POST request.
+    """
     payload = {
         '@timestamp': message.created_at.strftime('%Y-%m-%dT%H:%M:%SZ'),
         'author': {
@@ -58,40 +75,53 @@ async def post_message_to_elastic(message:discord.Message, verbose:bool=False) -
             'id': str(message.channel.id),
             'name': message.channel.name
         },
-        'attachments': [{'type': attachment.content_type, 'url': attachment.url } for attachment in message.attachments]
+        'attachments': [{'type': attachment.content_type, 'url': attachment.url} for attachment in message.attachments]
     }
-    logging.debug(f"Payload: {payload}")
+    logging.debug("Payload: %s", payload)
 
     resp = requests.post(
         url=f'{elastic_base_url}{elastic_index}/_doc',
         data=json.dumps(payload),
-        headers={'Content-Type': 'application/json','Authorization': f"ApiKey {elastic_token}"},
+        headers={'Content-Type': 'application/json',
+                 'Authorization': f"ApiKey {elastic_token}"},
         verify=False
     )
 
-    logging.debug(f"Response code {resp.status_code}: {resp.text}")
+    logging.debug("Response code %d: %s", resp.status_code, resp.text)
     if verbose:
         await message.channel.send(f"Payload: `{payload}`\n{resp.status_code}: `{resp.content}`")
 
 
 @client.event
 async def on_ready():
-    logging.info(f"Logged in as {client.user}")
+    """
+    Function automatically called when Discord client is ready.
+    """
+    logging.info("Logged in as %s", client.user)
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message) -> None:
+    """
+    Function automatically called when Discord client detects a message has been sent in a chat it can see.
+
+    params
+    ------
+    message : discord.Message
+        Discord message object of message sent in server.
+    """
     global verbose
     if message.author == client.user:
         logging.debug("Message sent by bot user, ignoring...")
         return
-    
-    if message.content is not None and message.content.startswith('!') and len(message.content) > 1 and message.author.id in superusers:
+
+    if message.content is not None and message.content.startswith('!') and \
+            len(message.content) > 1 and message.author.id in superusers:
         command = message.content[1:].split(' ')[0]
-        logging.info(f"Processing command {command}")
-        
+        logging.info("Processing command %s", command)
+
         if command == 'ingest':
-            logging.info(f"Ingesting messages from {message.channel.name}")
+            logging.info("Ingesting messages from %s", message.channel.name)
             await message.channel.send(f"Ingesting messages from {message.channel.name}...")
             message_count = 0
             async for historic_message in message.channel.history(limit=None):
@@ -99,16 +129,23 @@ async def on_message(message):
                     await post_message_to_elastic(historic_message)
                     message_count += 1
 
-            logging.info(f"Successfully ingested {message_count} messages from {message.channel.name}.")
+            logging.info("Successfully ingested %d messages from %s.",
+                         message_count, message.channel.name)
             await message.channel.send(f"Successfully ingested {message_count} messages from {message.channel.name}.")
             return
-        elif command == 'verbose':
-            verbose = False if verbose else True
-            logging.info(f"Verbose toggled to {verbose}.")
+        if command == 'verbose':
+            verbose = not verbose
+            logging.info("Verbose toggled to %s.", verbose)
             await message.channel.send(f"Verbose toggled to {verbose}.")
             return
 
-    await post_message_to_elastic(message, verbose)
+    await post_message_to_elastic(message)
 
 
-client.run(discord_token)
+def main():
+    """ Main method """
+    client.run(discord_token)
+
+
+if __name__ == '__main__':
+    main()
